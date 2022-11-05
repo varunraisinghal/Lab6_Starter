8d26b2d1290eb2d8e59b5093d8681f88e0757fab
varunraisinghal <varun.singhalacademic@yahoo.com>
2022-11-05 13:17:34 -0700

    Fixes

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -138,7 +138,7 @@ class RecipeCard extends HTMLElement {
   <p class="organization">${data.organization}</p>
   <div class="rating">
     <span>${data.rating}</span>
-    <img src="assets/images/icons/${data.rating}-star.svg" alt=${data.rating}>
+    <img src="https://varunraisinghal.github.io/Lab6_Starter/assets/images/icons/${data.rating}-star.svg" alt=${data.rating}>
     <span>${data.numRatings}</span>
   </div>
   <time>${data.lengthTime}</time>
